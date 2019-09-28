8f27ad3a00db0b21fbc2ee36394b1b91a72ec5c5
Kevin Cox <kevin.cox.work@gmail.com>
2019-09-27 20:29:41 -0400

    Added token request

--- src/app.js
+++ src/app.js
@@ -1,36 +1,66 @@
 const express = require("express");
-const request = require("request");
+var request = require("request");
+
 var SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 const port = 3000;
-const spotifyapi = require("spotify-web-api-node");
-var querystring = require("querystring");
-const my_client_id = "508fba76b5c3412db876cbe71f7be4ba";
-var client_secret = "5a5859da6071432a9d12f3d02c437f7b";
-const redirect_uri = "http://localhost:3000/";
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.get("/login", function(req, res) {
-  var scopes = ["user-read-private", "user-read-email"],
-    redirectUri = "https://localhost/",
+var scopes = ["user-read-private", "user-read-email"],
+    redirectUri = "http://localhost:3000/token",
     clientId = "508fba76b5c3412db876cbe71f7be4ba",
     state = "some-state-of-my-choice";
 
-  // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-  var spotifyApi = new SpotifyWebApi({
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+var spotifyApi = new SpotifyWebApi({
     redirectUri: redirectUri,
-    clientId: clientId
-  });
+    clientId: clientId,
+    clientSecret: "5a5859da6071432a9d12f3d02c437f7b"
+});
 
-  // Create the authorization URL
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+app.get("/", (req, res) => res.send("Hello World!"));
 
-  // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-  console.log(authorizeURL);
+app.get("/token", async (req, res) => {
 
-  //do a REST GET with this url.L0
-  //the returned connection url will have a CODE field with a repsonce
+    try {
+       // Retrieve an access token and a refresh token
+        await spotifyApi.authorizationCodeGrant(req.query.code).then(
+            function (data) {
+                console.log('The token expires in ' + data.body['expires_in']);
+                console.log('The access token is ' + data.body['access_token']);
+                console.log('The refresh token is ' + data.body['refresh_token']);
+
+                // Set the access token on the API object to use it in later calls
+                spotifyApi.setAccessToken(data.body['access_token']);
+                spotifyApi.setRefreshToken(data.body['refresh_token']);
+            },
+            function (err) {
+                console.log('Something went wrong!', err);
+            }
+        );
+
+        await spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+        .then(function (data) {
+            console.log('Artist albums', data.body);
+        }, function (err) {
+            console.error(err);
+        });
+
+
+        console.log(await spotifyApi.getMyDevices());
+
+    } catch (e) {
+        console.log(e);
+    }
+
+});
+
+app.get("/login", async (req, res) => {
+
+
+    // Create the authorization URL
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+    res.redirect(authorizeURL);
+    console.log(authorizeURL);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
